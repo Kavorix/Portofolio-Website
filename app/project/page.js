ee88e7dc551bfcb27ce6c23d4af6e8359310ec3b
kavorix <kavorix5@gmail.com>
2024-12-18 00:42:54 +0900

    fix error

--- app/project/page.js
+++ app/project/page.js
@@ -23,8 +23,8 @@ async function page() {
       <div className="flex justify-center my-5 lg:py-8">
         <div className="flex  items-center">
           <span className="w-24 h-[2px] bg-[#1a1443]"></span>
-          <span className="bg-[#1a1443] w-fit text-white p-2 px-5 whitespace-nowrap text-lg sm:text-2xl marker:rounded-md">
-            All PROJECTS
+          <span className="bg-[rgb(26,20,67)] w-fit text-white p-2 px-5 whitespace-nowrap text-lg sm:text-2xl marker:rounded-md">
+            PROJECTS
           </span>
           <span className="w-24 h-[2px] bg-[#1a1443]"></span>
         </div>
